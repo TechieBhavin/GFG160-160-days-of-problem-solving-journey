dd6a75e728b77415c16fc05b256b1ebf10e03e9f
Patel Bhavin <154894074+TechieBhavin@users.noreply.github.com>
2024-11-17 15:24:33 +0530

    Update solution.java

--- Day-3/solution.java
+++ Day-3/solution.java
@@ -1,1 +1,18 @@
+class Solution {
+    public void reverseArray(int arr[]) {
+        // code here
+         int left = 0;
+        int right = arr.length - 1;
 
+        while (left < right) {
+            // Swap elements
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            // Move pointers
+            left++;
+            right--;
+        }
+    }
+}
