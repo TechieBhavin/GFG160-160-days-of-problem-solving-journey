d7871cd6fae01f892c2bb27b7ee9cc49b8598a5f
Patel Bhavin <154894074+TechieBhavin@users.noreply.github.com>
2024-11-17 15:23:46 +0530

    Update solution.java

--- Day-1/solution.java
+++ Day-1/solution.java
@@ -1,1 +1,26 @@
-
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // Code Here
+        int lar = arr[0];
+        int tlar=-1;
+        int i;
+        for(i=1;i<arr.length;i++)
+        {
+            if(arr[i]>lar)
+            {
+                lar=arr[i];
+            }
+        }
+        for(i=0;i<arr.length;i++)
+        {
+            if(lar!=arr[i])
+            {
+                if(arr[i]>tlar)
+                {
+                    tlar=arr[i];
+                }
+            }
+        }
+        return tlar;
+    }
+}
