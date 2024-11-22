91372f1eeabf0622b8349040ceff7aaea1781641
Patel Bhavin <154894074+TechieBhavin@users.noreply.github.com>
2024-11-22 14:45:04 +0530

    Update solution.java

--- Day-8/solution.java
+++ Day-8/solution.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int maximumProfit(int prices[]) {
+        // Code here
+        int ans=0,st=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<prices[st]){
+                st=i;
+            }
+            else{
+                ans=Math.max(ans, prices[i]-prices[st]);
+            }
+        }
+        return ans;
+    }
+}
