67e5e0a6f342204573cf7fefe6105897757a7ae6
Patel Bhavin <154894074+TechieBhavin@users.noreply.github.com>
2024-11-22 14:47:19 +0530

    Update solution.java

--- Day-4/solution.java
+++ Day-4/solution.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    // Function to rotate an array by d elements in counter-clockwise direction.
+    static void rotateArr(int arr[], int d) {
+       ArrayList<Integer> nums = new ArrayList<>();
+        for(int i = 0; i < arr.length; i++){
+            nums.add(arr[i]);
+        }
+        d = d % nums.size();
+        Collections.reverse(nums.subList(0, d));
+        Collections.reverse(nums.subList(d, nums.size()));
+        Collections.reverse(nums);
+        for(int i = 0; i < nums.size(); i++){
+            arr[i] = nums.get(i);
+        }
+    }
+}
