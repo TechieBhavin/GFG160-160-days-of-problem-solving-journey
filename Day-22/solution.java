a1044e18596b1f0384d6430244beab4e6bc7ec74
Patel Bhavin <154894074+TechieBhavin@users.noreply.github.com>
2024-12-06 21:42:33 +0530

    Update solution.java

--- Day-22/solution.java
+++ Day-22/solution.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    // Function to find hIndex
+    public int hIndex(int[] citations) {
+        // code here
+        int n=citations.length;
+        int max = 0;
+        Arrays.sort(citations);
+        for(int i=n-1; i>=0; i--){
+            if(n-i<=citations[i])
+            max=Math.max(max, n-i);
+        }
+        return max;
+    }
+}
