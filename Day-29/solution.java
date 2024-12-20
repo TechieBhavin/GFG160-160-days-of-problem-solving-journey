1e6fb95c50624f97ad8a603d6779c7c46a4f16d4
Patel Bhavin <154894074+TechieBhavin@users.noreply.github.com>
2024-12-20 10:48:34 +0530

    Update solution.java

--- Day-29/solution.java
+++ Day-29/solution.java
@@ -1,1 +1,14 @@
 
+class Solution {
+    public int findMin(int[] arr) {
+        // complete the function here
+         int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]>arr[end]) start=mid+1;
+            else end=mid;
+        }
+        return arr[start];
+    }
+}
