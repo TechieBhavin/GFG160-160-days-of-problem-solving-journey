af1cdb5e386f93ac11b8dea1a060d8e20d5a5a02
Patel Bhavin <154894074+TechieBhavin@users.noreply.github.com>
2024-12-20 10:51:10 +0530

    Update solution.java

--- Day-35/solution.java
+++ Day-35/solution.java
@@ -1,1 +1,21 @@
+class Solution {
+    public int kthMissing(int[] arr, int k) {
+        int missingCount = 0;  // Count of missing numbers so far
+        int prev = 0;         // Previous number, starts at 0
+        
+        for (int num : arr) {
+            // Count missing numbers between prev and current number
+            missingCount += num - prev - 1;
 
+            // If kth missing is between prev and num
+            if (missingCount >= k) {
+                return num - (missingCount - k + 1);
+            }
+            
+            prev = num; // Update previous number
+        }
+        
+        // If kth missing is beyond the last array element
+        return arr[arr.length - 1] + k - missingCount;
+    }
+}
