8fde91eadddea0c65f7b90ea6c8402bd614093fc
Patel Bhavin <154894074+TechieBhavin@users.noreply.github.com>
2024-12-20 10:47:45 +0530

    Update solution.java

--- Day-27/solution.java
+++ Day-27/solution.java
@@ -1,1 +1,27 @@
-
+class Solution {
+    // Function to merge the arrays.
+    public void mergeArrays(int a[], int b[]) {
+    
+        
+        int m = a.length;
+        int n = b.length;
+        
+        int left = m-1; 
+        int right = 0;
+        
+        while(left >=0 && right < n && a[left] > b[right]){
+           
+                
+                int temp = a[left];
+                a[left] = b[right];
+                b[right] = temp;
+            
+                left --;
+                right ++;
+        }
+            
+            
+            Arrays.sort(a);
+            Arrays.sort(b);
+    }
+}
