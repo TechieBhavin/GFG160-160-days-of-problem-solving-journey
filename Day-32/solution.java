4b8fdeac42d248c16f0865989de7e97729158c04
Patel Bhavin <154894074+TechieBhavin@users.noreply.github.com>
2024-12-20 10:49:49 +0530

    Update solution.java

--- Day-32/solution.java
+++ Day-32/solution.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public int kthElement(int a[], int b[], int k) {
+        // code here
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int i:a)
+        list.add(i);
+        for(int i:b)
+        list.add(i);
+        Collections.sort(list);
+        int ele=list.get(k-1);
+        return ele;
+    }
+}
