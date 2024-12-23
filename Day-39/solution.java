de9101f87ed54ece3017a1b45094d6be309285c6
Patel Bhavin <154894074+TechieBhavin@users.noreply.github.com>
2024-12-23 11:52:23 +0530

    Update solution.java

--- Day-39/solution.java
+++ Day-39/solution.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    // Function to search a given number in row-column sorted matrix.
+    public boolean searchRowMatrix(int[][] mat, int x) {
+        // code here
+        // for (int row[]: mat) if (Arrays.binarySearch(row, x) >= 0) 
+        //     return true;
+        // return false;
+        int n = mat.length, m = mat[0].length;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(mat[i][j]==x)
+                    return true;
+            }
+        }
+        return false;
+    }
+}
