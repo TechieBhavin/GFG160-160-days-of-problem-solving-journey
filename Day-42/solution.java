2870132ba5f748044fdc27cdfce4d4d6bc21b95b
Patel Bhavin <154894074+TechieBhavin@users.noreply.github.com>
2024-12-26 11:05:28 +0530

    Update solution.java

--- Day-42/solution.java
+++ Day-42/solution.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    boolean twoSum(int arr[], int target) {
+        // code here
+          HashSet<Integer> set = new HashSet<>();
+      for(int val : arr){
+          if(set.contains(target-val)){
+              return true;
+          }
+          set.add(val);
+      }
+      return false;
+    }
+}
