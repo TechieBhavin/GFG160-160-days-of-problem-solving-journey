a8c011c16705e6d6549efd8079bedd0501baa043
Patel Bhavin <154894074+TechieBhavin@users.noreply.github.com>
2024-12-29 11:03:00 +0530

    Update solution.java

--- Day-45/solution.java
+++ Day-45/solution.java
@@ -1,1 +1,18 @@
 
+class Solution {
+    public ArrayList<Integer> intersectionWithDuplicates(int[] a, int[] b) {
+        // code here
+        Set<Integer>set = new HashSet<>();
+        for(int num:a){
+            set.add(num);
+        }
+      ArrayList<Integer> list = new ArrayList<>();
+      for(int i = 0 ; i<b.length ; i++){
+          if(set.contains(b[i])){
+              list.add(b[i]);
+              set.remove(b[i]);
+          }
+      }
+      return list ; 
+    }
+}
