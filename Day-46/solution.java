c2e70cb7acad816c5796af90754a922a1af9bc0f
Patel Bhavin <154894074+TechieBhavin@users.noreply.github.com>
2024-12-30 12:39:48 +0530

    Update solution.java

--- Day-46/solution.java
+++ Day-46/solution.java
@@ -1,1 +1,10 @@
-
+class Solution {
+    public static int findUnion(int a[], int b[]) {
+        // code here
+             Set<Integer> set = new HashSet<>();
+        for(int ele:a) set.add(ele);
+        for(int ele:b) set.add(ele);
+        
+        return set.size();
+    }
+}
